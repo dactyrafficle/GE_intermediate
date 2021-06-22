7fbb7325cec4057d00c14589346c7cb1ce54d2c0
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-06-21 20:46:47 -0400

    Add files via upload

---
+++ exogenous_variables.js
@@ -0,0 +1,5 @@
+// EXOGENOUS
+let alpha = 0.5;
+let beta = 0.5; // alpha = 0.6, beta = 0.4 is good. if alpha=beta, then for 1 unit of labor, u can make the same amt of capital as goods
+let gamma = 1.1; // leisure preference
+let L = 24;
---
+++ initial_prices.js
@@ -0,0 +1,4 @@
+
+// ENDOGENOUS
+let w = 1;
+let p = [1, 1];
